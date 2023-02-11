9a0d590720e9f2fc4f053943f4f319c51cc2e496
Chattria T. Palm <86838286+chattria@users.noreply.github.com>
2023-02-11 21:11:41 +0100

    Theme
    Removed the  holo and terminal button.
    Added Js to the blue screen mode and console mode to work.

--- blue_screen_src/blue.js
+++ blue_screen_src/blue.js
@@ -1,3 +1,28 @@
+const blueBtn = document.querySelector("#blue-screen");
+const whiteBtn = document.querySelector("#console");
+
+blueBtn.addEventListener("click", () => {
+    document.documentElement.style.setProperty('--first-background-color', '#0007C9');
+    document.documentElement.style.setProperty('--first-text-color', '#fff');
+    document.documentElement.style.setProperty('--first-shadow', '1px 1px 2px #fff', '0 0 0.1rem #fff', '0 0 0.1rem #fff');
+    document.documentElement.style.setProperty('--border-btn', '1px solid #fff');
+    document.documentElement.style.setProperty('--border', '2px solid #fff');
+    document.documentElement.style.setProperty('--second-background-color', '#fff');
+    document.documentElement.style.setProperty('--second-text-color', '#0007C9');
+    document.documentElement.style.setProperty('--second-shadow', '1px 1px 2px  #0007C9', '0 0 0.1rem  #0007C9', '0 0 0.1rem  #0007C9');
+});
+
+whiteBtn.addEventListener("click", () => {
+    document.documentElement.style.setProperty('--first-background-color', '#000000');
+    document.documentElement.style.setProperty('--first-text-color', '#fff');
+    document.documentElement.style.setProperty('--first-shadow', '1px 1px 2px #fff', '0 0 0.1rem #fff', '0 0 0.1rem #fff');
+    document.documentElement.style.setProperty('--border-btn', '1px solid #fff');
+    document.documentElement.style.setProperty('--border', '2px solid #fff');
+    document.documentElement.style.setProperty('--second-background-color', '#fff');
+    document.documentElement.style.setProperty('--second-text-color', '#000000');
+    document.documentElement.style.setProperty('--second-shadow', '1px 1px 2px  #000000', '0 0 0.1rem  #000000', '0 0 0.1rem  #000000');
+});
+
 //6 day forcast
 let dates = new Date();
 let shortDays = [
